e1354a564880532f0d7284076f1d56a717ea64ca
Dominik Gałenza <dominik.galenza@gmail.com>
2024-07-28 14:08:44 +0200

    Created basic classes and blueprints.

---
+++ Source/ARPG/Public/Characters/ARPGHeroCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/ARPGBaseCharacter.h"
+#include "ARPGHeroCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API AARPGHeroCharacter : public AARPGBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
