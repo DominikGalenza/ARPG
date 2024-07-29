7ed2ddb32b2d7f508ea4ce238d99ca7bd6a8bb97
Dominik Gałenza <dominik.galenza@gmail.com>
2024-07-29 20:01:58 +0200

    Created a custom enhanced input component.

---
+++ Source/ARPG/Public/Components/Input/ARPGInputComponent.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+#include "ARPGInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UARPGInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+public:
+	template<class UserObject, typename CallbackFunc>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func);
+};
+
+template<class UserObject, typename CallbackFunc>
+inline void UARPGInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func)
+{
+	checkf(InInputConfig, TEXT("Input config data is null, can not proceed with binding"));
+
+	if (UInputAction* FoundAction = InInputConfig->FindNativeInputActionByTag(InInputTag))
+	{
+		BindAction(FoundAction, TriggerEvent, ContextObject, Func);
+	}
+}
